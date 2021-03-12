68de094441acb12029a2957541ac56a6c6cde99a
jschollitt <67741312+jschollitt@users.noreply.github.com>
2021-03-12 17:34:44 +1300

    Added Week 2 project
    Week 2 Program contains static method with FizzBuzz implementation. LinkedList file contains the Node class and LinkedList class with AddNode method implemented.

---
+++ 6426-1822/Week2/LinkedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week2
+{
+    /// <summary>
+    /// Node class. Responsible for holding a value and
+    /// a reference to another node. That's it.
+    /// </summary>
+    public class LLNode
+    {
+        public int value;
+        public LLNode next;
+
+        public LLNode(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    /// <summary>
+    /// Data type class. Handles the creation, manipulation,
+    /// modification and removal of values.
+    /// </summary>
+    public class LinkedList
+    {
+        /// The entry point of the list
+        public LLNode start;
+
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
+        public LinkedList()
+        {
+        }
+
+        /// <summary>
+        /// Helper method that takes an int and adds it
+        /// to the list in the form of a node
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddNode(int value)
+        {
+            /// new node to be added
+            LLNode newNode = new LLNode(value);
+
+            /// If no nodes have been added yet set the
+            /// start of the list to the new node
+            if (this.start == null)
+                this.start = newNode;
+
+            /// A node has been added already, find
+            /// the end of the chain
+            else
+            {
+                /// Create a reference to the starting node
+                LLNode traversalNode = this.start;
+
+                /// Until we get to the end of the chain
+                while (traversalNode.next != null)
+                {
+                    /// Move the reference to the next node
+                    /// in the chain
+                    traversalNode = traversalNode.next;
+                }
+
+                /// Once the while loop finishes, we will be
+                /// at the end of the chain.
+
+                /// Point the last node in the chain to the
+                /// newly-created node, adding it to our chain
+                traversalNode.next = newNode;
+            }
+        }
+
+    }
+}
---
+++ 6426-1822/Week2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FizzBuzz();
+
+            LinkedList myLinkedList = new LinkedList();
+            
+            /// Enumerable.Range is available in System.Linq namespace.
+            /// Remember to include it at the top of your file.
+            foreach (int i in Enumerable.Range(1, 10))
+            {
+                myLinkedList.AddNode(i);
+            }
+        }
+
+        /// <summary>
+        /// FizzBuzz method.
+        /// Uses the static keyword to allow the method to be called
+        /// without needing an instance of the class
+        /// </summary>
+        public static void FizzBuzz()
+        {
+            /// Ask for a number from user
+            Console.WriteLine("Enter a number:");
+
+            /// Take the input, save to a local string var
+            string input = Console.ReadLine();
+
+            /// Convert string to int
+            int inputNum = Convert.ToInt32(input);
+
+            /// For each int from 1 to given number
+            for (int i = 1; i < inputNum; i++)
+            {
+                /// If i is multiple of 3 and 5
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+
+                /// If not multiple of both, is multiple of just 3
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+
+                /// If not multiple of both, is multiple of just 5
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+
+                /// If none of the above, print the number
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
